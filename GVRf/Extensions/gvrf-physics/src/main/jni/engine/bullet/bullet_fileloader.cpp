ad81ccc6dfb4c331c1f2c7efa36483ab77aafa2b
Cristiano Bozzetto <c.bozzetto@samsung.com>
2018-04-10 14:17:59 -0400

    Dealing with differences among exporters
    - Rigid bodies w/o name will be ignored;
    - Non supported constraints will be ignored;
    - Will rotate constraints pivots if exporter uses Z as up axis.
    
    GearVRf-DCO-1.0-Signed-off-by: Cristiano Bozzetto <c.bozzetto@samsung.com>

--- GVRf/Extensions/gvrf-physics/src/main/jni/engine/bullet/bullet_fileloader.cpp
+++ GVRf/Extensions/gvrf-physics/src/main/jni/engine/bullet/bullet_fileloader.cpp
@@ -15,6 +15,8 @@
 #include <BulletDynamics/ConstraintSolver/btPoint2PointConstraint.h>
 #include <BulletDynamics/ConstraintSolver/btSliderConstraint.h>
 #include <BulletDynamics/ConstraintSolver/btSequentialImpulseConstraintSolver.h>
+#include <Serialize/BulletFileLoader/btBulletFile.h>
+#include <BulletDynamics/Dynamics/btDynamicsWorld.h>
 
 #include "bullet_fileloader.h"
 #include "bullet_world.h"
@@ -33,95 +35,107 @@ static btTransform transformInvIdty(matrixInvIdty);
 
 namespace gvr {
 
-static btBulletWorldImporter *parse_buffer(char *buf, int len, btDynamicsWorld *world) {
-    btBulletWorldImporter *importer = new btBulletWorldImporter(world);
-    importer->loadFileFromMemory(buf, len);
-
-    return importer;
-}
-
 static void createBulletRigidBodies(btBulletWorldImporter *importer)
 {
     for (int i = 0; i < importer->getNumRigidBodies(); i++)
     {
         btRigidBody *rb = reinterpret_cast<btRigidBody*>(importer->getRigidBodyByIndex(i));
 
+        if (nullptr == importer->getNameForPointer(rb))
+        {
+            // A rigid body has no name.
+            // TODO: check if this body can be safely deleted
+            continue;
+        }
+
         // btRigidBody userPointer will point to the newly created BulletRigidBody
         BulletRigidBody *brb = new BulletRigidBody(rb);
     }
 }
 
-static void createBulletP2pConstraint(btPoint2PointConstraint *p2p)
+static void createBulletP2pConstraint(btPoint2PointConstraint *p2p, bool needRotate)
 {
     // Constraint userPointer will point to newly created BulletPoint2PointConstraint
     BulletPoint2PointConstraint *bp2p = new BulletPoint2PointConstraint(p2p);
 
     __android_log_print(ANDROID_LOG_DEBUG, tag, "Created point-to-point constraint");
 
-    // Adapting pivot to GVRf coordinates system
-    btVector3 pivot = p2p->getPivotInA();
-    float t = pivot.getZ();
-    pivot.setZ(-pivot.getY());
-    pivot.setY(t);
-    p2p->setPivotA(pivot);
+    if (needRotate)
+    {
+        // Adapting pivot to GVRf coordinates system
+        btVector3 pivot = p2p->getPivotInA();
+        float t = pivot.getZ();
+        pivot.setZ(-pivot.getY());
+        pivot.setY(t);
+        p2p->setPivotA(pivot);
 
-    pivot = p2p->getPivotInB();
-    t = pivot.getZ();
-    pivot.setZ(-pivot.getY());
-    pivot.setY(t);
-    p2p->setPivotB(pivot);
+        pivot = p2p->getPivotInB();
+        t = pivot.getZ();
+        pivot.setZ(-pivot.getY());
+        pivot.setY(t);
+        p2p->setPivotB(pivot);
+    }
 }
 
-static void createBulletHingeConstraint(btHingeConstraint *hg)
+static void createBulletHingeConstraint(btHingeConstraint *hg, bool needRotate)
 {
     BulletHingeConstraint *bhg = new BulletHingeConstraint(hg);
 
     __android_log_print(ANDROID_LOG_DEBUG, tag, "Created hinge constraint");
 
-    btTransform t = hg->getAFrame();
-    hg->getAFrame().mult(transformInvIdty, t);
+    if (needRotate)
+    {
+        btTransform t = hg->getAFrame();
+        hg->getAFrame().mult(transformInvIdty, t);
 
-    t = hg->getBFrame();
-    hg->getBFrame().mult(transformInvIdty, t);
+        t = hg->getBFrame();
+        hg->getBFrame().mult(transformInvIdty, t);
+    }
 }
 
-static void createBulletConeTwistConstraint(btConeTwistConstraint *ct)
+static void createBulletConeTwistConstraint(btConeTwistConstraint *ct, bool needRotate)
 {
     BulletConeTwistConstraint *bct = new BulletConeTwistConstraint(ct);
 
     __android_log_print(ANDROID_LOG_DEBUG, tag, "Created cone twist constraint");
 
-    btTransform tA = ct->getAFrame();
-    btTransform tB = ct->getBFrame();
+    if (needRotate)
+    {
+        btTransform tA = ct->getAFrame();
+        btTransform tB = ct->getBFrame();
 
-    btTransform t = tA;
-    tA.mult(transformInvIdty, t);
+        btTransform t = tA;
+        tA.mult(transformInvIdty, t);
 
-    t = tB;
-    tB.mult(transformInvIdty, t);
+        t = tB;
+        tB.mult(transformInvIdty, t);
 
-    ct->setFrames(tA, tB);
+        ct->setFrames(tA, tB);
+    }
 }
 
-static void createBulletGenericConstraint(btGeneric6DofConstraint *gen)
+static void createBulletGenericConstraint(btGeneric6DofConstraint *gen, bool needRotate)
 {
     BulletGeneric6dofConstraint *bct = new BulletGeneric6dofConstraint(gen);
 
     __android_log_print(ANDROID_LOG_DEBUG, tag, "Created generic 6DoF constraint");
 
-    btTransform tA = gen->getFrameOffsetA();
-    btTransform tB = gen->getFrameOffsetB();
+    if (needRotate)
+    {
+        btTransform tA = gen->getFrameOffsetA();
+        btTransform tB = gen->getFrameOffsetB();
 
-    btTransform t = tA;
-    tA.mult(transformInvIdty, t);
+        btTransform t = tA;
+        tA.mult(transformInvIdty, t);
 
-    t = tB;
-    tB.mult(transformInvIdty, t);
+        t = tB;
+        tB.mult(transformInvIdty, t);
 
-    gen->setFrames(tA, tB);
+        gen->setFrames(tA, tB);
+    }
 }
 
-static void createBulletConstraints(btBulletWorldImporter *importer)
+static void createBulletConstraints(btBulletWorldImporter *importer, bool needRotate)
 {
     for (int i = 0; i < importer->getNumConstraints(); i++)
     {
@@ -131,21 +145,21 @@ static void createBulletConstraints(btBulletWorldImporter *importer)
 
         if (constraint->getConstraintType() == btTypedConstraintType::POINT2POINT_CONSTRAINT_TYPE)
         {
-            createBulletP2pConstraint(static_cast<btPoint2PointConstraint*>(constraint));
+            createBulletP2pConstraint(static_cast<btPoint2PointConstraint*>(constraint), needRotate);
         }
         else if (constraint->getConstraintType() == btTypedConstraintType::HINGE_CONSTRAINT_TYPE)
         {
-            createBulletHingeConstraint(static_cast<btHingeConstraint*>(constraint));
+            createBulletHingeConstraint(static_cast<btHingeConstraint*>(constraint), needRotate);
         }
         else if (constraint->getConstraintType() == btTypedConstraintType::CONETWIST_CONSTRAINT_TYPE)
         {
-            createBulletConeTwistConstraint(static_cast<btConeTwistConstraint*>(constraint));
+            createBulletConeTwistConstraint(static_cast<btConeTwistConstraint*>(constraint), needRotate);
         }
         else if (constraint->getConstraintType() == btTypedConstraintType::D6_CONSTRAINT_TYPE ||
                  constraint->getConstraintType() == btTypedConstraintType::D6_SPRING_CONSTRAINT_TYPE)
         {
             // Blender exports generic constraint as generic spring constraint
-            createBulletGenericConstraint(static_cast<btGeneric6DofConstraint*>(constraint));
+            createBulletGenericConstraint(static_cast<btGeneric6DofConstraint*>(constraint), needRotate);
         }
     }
 
@@ -154,10 +168,32 @@ static void createBulletConstraints(btBulletWorldImporter *importer)
 BulletFileLoader::BulletFileLoader(char *buffer, size_t length) :
     PhysicsLoader(buffer, length), mCurrRigidBody(0), mCurrConstraint(0)
 {
-    mImporter = parse_buffer(buffer, length, nullptr);
+    bParse::btBulletFile *bullet_file = new bParse::btBulletFile(buffer, length);
+
+    mImporter = new btBulletWorldImporter(nullptr);
+    mImporter->loadFileFromMemory(bullet_file);
+
+    bool needRotate;
+    if (bullet_file->getFlags() & bParse::FD_DOUBLE_PRECISION)
+    {
+        btDynamicsWorldDoubleData* ddata =
+                reinterpret_cast<btDynamicsWorldDoubleData*>(bullet_file->m_dynamicsWorldInfo[0]);
+        double *gravity = reinterpret_cast<double *>(&ddata->m_gravity);
+        needRotate = gravity[2] != 0.0;
+    }
+    else
+    {
+        btDynamicsWorldFloatData* fdata =
+                reinterpret_cast<btDynamicsWorldFloatData*>(bullet_file->m_dynamicsWorldInfo[0]);
+        float *gravity = reinterpret_cast<float*>(&fdata->m_gravity);
+        needRotate = gravity[2] != 0.f;
+    }
+
+    delete bullet_file;
+
     createBulletRigidBodies(mImporter);
 
-    createBulletConstraints(mImporter);
+    createBulletConstraints(mImporter, needRotate);
 }
 
 BulletFileLoader::~BulletFileLoader()
@@ -169,11 +205,20 @@ PhysicsRigidBody* BulletFileLoader::getNextRigidBody()
 {
     PhysicsRigidBody *ret = nullptr;
 
-    if (mCurrRigidBody < mImporter->getNumRigidBodies())
+    while (mCurrRigidBody < mImporter->getNumRigidBodies())
     {
-        void *brb = mImporter->getRigidBodyByIndex(mCurrRigidBody)->getUserPointer();
-        ret = reinterpret_cast<BulletRigidBody*>(brb);
+        btRigidBody *rb = static_cast<btRigidBody*>(mImporter->getRigidBodyByIndex(mCurrRigidBody));
         ++mCurrRigidBody;
+
+        // This rigid body has no wrapper and then must be ignored
+        if (nullptr != rb->getUserPointer())
+        {
+            ret = reinterpret_cast<BulletRigidBody*>(rb->getUserPointer());
+            __android_log_print(ANDROID_LOG_DEBUG, tag, "Sending rigid body %i [%p %p]",
+                                mCurrConstraint - 1, ret, rb);
+            break;
+        }
+        // TODO: check if an ignored rigid body can be safely deleted
     }
 
     return ret;
@@ -190,15 +235,19 @@ PhysicsConstraint* BulletFileLoader::getNextConstraint()
 {
     PhysicsConstraint *ret = nullptr;
 
-    if (mCurrConstraint < mImporter->getNumConstraints())
+    while (mCurrConstraint < mImporter->getNumConstraints())
     {
-        void *phc = mImporter->getConstraintByIndex(mCurrConstraint)->getUserConstraintPtr();
-
-        ret = static_cast<PhysicsConstraint*>(phc);
-        __android_log_print(ANDROID_LOG_DEBUG, tag, "Sending constraint %i [%p / %p]",
-            mCurrConstraint, phc, mImporter->getConstraintByIndex(mCurrConstraint));
-
+        btTypedConstraint *constraint = mImporter->getConstraintByIndex(mCurrConstraint);
         ++mCurrConstraint;
+
+        // Will ignore non supported constraints
+        if (nullptr != constraint->getUserConstraintPtr() && ((void*)-1) != constraint->getUserConstraintPtr())
+        {
+            ret = static_cast<PhysicsConstraint *>(constraint->getUserConstraintPtr());
+            __android_log_print(ANDROID_LOG_DEBUG, tag, "Sending constraint %i [%p / %p]",
+                                mCurrConstraint - 1, ret, constraint);
+            break;
+        }
     }
 
     return ret;
