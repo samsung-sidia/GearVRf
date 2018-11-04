e5238f532af1b5e263737497b5291611cdbaf269
NolaDonato <nola.donato@samsung.com>
2018-11-03 21:10:00 -0700

    Update GVRf/Framework/framework/src/main/jni/objects/scene.cpp
    fixed typo in copyright in scene.cpp

--- GVRf/Framework/framework/src/main/jni/objects/scene.cpp
+++ GVRf/Framework/framework/src/main/jni/objects/scene.cpp
@@ -1,5 +1,4 @@
 /* Copyright 2015 Samsung Electronics Co., LTD
-A/* Copyright 2015 Samsung Electronics Co., LTD
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
